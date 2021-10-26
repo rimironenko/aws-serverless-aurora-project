9413b84e219b4e7389633cd3100f9db633f29e16
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-10-26 14:32:46 +0300

    Initial commit

---
+++ src/main/java/com/home/amazon/serverless/DependencyFactory.java
@@ -0,0 +1,26 @@
+
+package com.home.amazon.serverless;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rds.RdsClient;
+
+/**
+ * The module containing all dependencies required by the {@link App}.
+ */
+public class DependencyFactory {
+
+    private DependencyFactory() {}
+
+    /**
+     * @return an instance of RdsClient
+     */
+    public static RdsClient rdsClient() {
+        return RdsClient.builder()
+                       .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                       .region(Region.US_EAST_1)
+                       .httpClientBuilder(UrlConnectionHttpClient.builder())
+                       .build();
+    }
+}
