ecbde6aff6a957dfc333afb5b55fa1d5449cdd41
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-10-27 16:34:46 +0300

    Updated permissions

--- src/main/java/com/home/amazon/serverless/lambda/GetAuroraItemFunction.java
+++ src/main/java/com/home/amazon/serverless/lambda/GetAuroraItemFunction.java
@@ -4,8 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.home.amazon.serverless.DependencyFactory;
 import software.amazon.awssdk.services.rdsdata.RdsDataClient;
-import software.amazon.awssdk.services.rdsdata.model.ExecuteStatementRequest;
-import software.amazon.awssdk.services.rdsdata.model.ExecuteStatementResponse;
+import software.amazon.awssdk.services.rdsdata.model.*;
 
 /**
  * Lambda function entry point. You can change to use other pojo type or implement
@@ -14,6 +13,8 @@ import software.amazon.awssdk.services.rdsdata.model.ExecuteStatementResponse;
  * @see <a href=https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html>Lambda Java Handler</a> for more information
  */
 public class GetAuroraItemFunction implements RequestHandler<Object, String> {
+
+    private static final String GET_BY_ID_SQL_STATEMENT = "select * from %s.books";
 
     private final RdsDataClient rdsDataClient;
     private final String auroraClusterArn;
@@ -28,8 +29,14 @@ public class GetAuroraItemFunction implements RequestHandler<Object, String> {
 
     @Override
     public String handleRequest(Object input, Context context) {
-        ExecuteStatementRequest request = ExecuteStatementRequest.builder().resourceArn(auroraClusterArn).database(auroraDatabase).build();
+        ExecuteStatementRequest request = ExecuteStatementRequest.builder()
+                .database(auroraDatabase)
+                .resourceArn(auroraClusterArn)
+                .sql(String.format(GET_BY_ID_SQL_STATEMENT, auroraDatabase))
+                //.parameters(SqlParameter.builder().name("id").value(Field.builder().stringValue("1").build()).build())
+                .build();
         ExecuteStatementResponse executeStatementResponse = rdsDataClient.executeStatement(request);
+        System.out.println(executeStatementResponse);
         return null;
     }
 }
