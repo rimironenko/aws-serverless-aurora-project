f0fe19dba047a9305880308ee0a9f3d3ff77387d
Rostyslav_Myronenko <rimironenko@gmail.com>
2021-10-27 19:16:28 +0300

    Updated Lambda Permission

--- database.sql
+++ database.sql
@@ -1,13 +1,14 @@
+delimiter @
+
 create database if not exists booksdb
-
+@
 drop table if exists booksdb.books
-
+@
 create table booksdb.books (
     id INT NOT NULL AUTO_INCREMENT,
     name varchar(255) CHARACTER SET utf8 NOT NULL,
     author varchar(255) CHARACTER SET utf8 NOT NULL,
     PRIMARY KEY (id)
 )
-
+@
 insert into booksdb.books values (1, 'Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin')
-
